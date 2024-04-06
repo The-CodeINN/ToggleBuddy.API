4b783309e3815133b6d8bfc2d8f431115d16efc5
oluwafemi <oluwafemioluwatuyi8@gmail.com>
2024-04-06 16:46:40 +0100

    Added the User model and Authentication setup

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToggleBuddy.API.Models.DTOs.RequestDTOs;
+using ToggleBuddy.API.Models.DTOs.ResponseDTOs;
+using ToggleBuddy.API.Respositories.Implementations;
+using ToggleBuddy.API.Respositories.Interfaces;
+
+namespace ToggleBuddy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ITokenRepository tokenRepository;
+
+        public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
+        {
+            this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
+        }
+        [HttpPost]
+        [Route("Register")]
+
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            { 
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+          var identityResult =  await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if(identityResult.Succeeded)
+            { 
+                //Add roles to the user
+                if(registerRequestDto.Roles != null&& registerRequestDto.Roles.Any())
+                await userManager.AddPasswordAsync(identityUser, registerRequestDto.Roles);
+                if(identityResult.Succeeded)
+                {
+                    return Ok("User was reg");
+                }
+            }
+
+            return BadRequest("wrong");
+        }
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
+
+            if (user != null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+                if (checkPasswordResult)
+                {
+                    //Get roles for this user
+                    var roles = await userManager.GetRolesAsync(user);
+
+                    if (roles != null)
+                    {
+                        //Create Token
+                        var jwtToken = tokenRepository.CreateJWTToken(user, roles.ToList());
+
+                        var response = new LoginResponseDto
+                        {
+                            JwtToken = jwtToken,
+                        };
+                        return Ok(response);
+                    }
+
+               
+
+
+
+                }
+            }
+
+            return BadRequest("incorrect");
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using ToggleBuddy.API.Data;
+using ToggleBuddy.API.Respositories.Implementations;
+using ToggleBuddy.API.Respositories.Interfaces;
 
 namespace ToggleBuddy.API
 {
@@ -20,6 +26,45 @@ namespace ToggleBuddy.API
             // adding database
             builder.Services.AddDbContext<ToggleBuddyDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("ToggleBuddyConnectionString")));
+            // adding database for auth
+            builder.Services.AddDbContext<ToggleBuddyAuthDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ToggleBuddyAuthConnectionString")));
+
+            builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+            // Configure ASP.NET Core Identity services
+            builder.Services.AddIdentityCore<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("ToggleBuddy")
+                .AddEntityFrameworkStores<ToggleBuddyAuthDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Customize password requirements
+                options.Password.RequireDigit = true;
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+               
+            });
+            // adding authentication configuration
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // Validate the audience (recipient) of the token
+                    ValidateAudience = true,
+                    // Validate the issuer (who issued the token)
+                    ValidateIssuer = true,
+                    // Validate the token's expiration time
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"], 
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                });
 
             var app = builder.Build();
 
@@ -31,7 +76,7 @@ namespace ToggleBuddy.API
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
