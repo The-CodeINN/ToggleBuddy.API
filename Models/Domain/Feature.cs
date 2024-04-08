7e4377fd47a7187ada0431b0b582958f04230959
Richard Emijere <emijere.richard@gmail.com>
2024-04-08 14:31:05 +0100

    added feature model

--- Models/Domain/Feature.cs
+++ Models/Domain/Feature.cs
@@ -1,6 +1,17 @@
 ﻿namespace ToggleBuddy.API.Models.Domain
 {
-    public class Features
+    public class Feature
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Guid ProjectId { get; set; }
+
+        // Navigation properties
+        public Project Project { get; set; }
+
     }
 }
